2ef860e1558df4fcebfddb741f69addace8800dc
Tomal <tanjimahmed450@gmail.com>
2026-09-26 16:02:30 +0600

    Add dashboard UI

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const express = require("express");
+const path = require("path");
 
 const monitoring = require("./monitoring");
 
 function createApp() {
   const app = express();
   app.use(express.json());
+  app.use(express.static(path.join(__dirname, "public")));
 
   app.get("/health", (req, res) => {
     res.json({ status: "ok" });
